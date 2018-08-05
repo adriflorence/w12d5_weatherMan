09a6503db8a852f682087d61b537cd619c11898a
Adri <forczekadri@gmail.com>
2018-08-05 23:01:53 +0100

    places weather icon as marker on map

--- public/app.js
+++ public/app.js
@@ -1,35 +1,42 @@
 const makeRequest = function (url, callback) {
   const request = new XMLHttpRequest();
   request.open("GET", url);
-  request.addEventListener('load', callback);
+  request.onreadystatechange = function () { // when state changes
+    if(request.readyState === 4 && request.status === 200) { // 4: operation complete (returned)
+      callback(JSON.parse(request.responseText));
+    }
+  }
   request.send();
 };
 
-const requestComplete = function () {
-  if(this.status !== 200) return;
-  const jsonString = this.responseText;
-  const weatherStuff = JSON.parse(jsonString);
-  console.log(weatherStuff);
-  displayWeather(weatherStuff);
-};
-
-const displayWeather = function (weatherStuff) {
-  weatherStuff.consolidated_weather.forEach(function(location) {
-
-    console.log(location);
+const fetchWeatherData = function (response) {
+  const woeid = response[0].woeid; // where on earth ID
+  makeRequest(`https://www.metaweather.com/api/location/${woeid}`, (response) => {
+    // console.log(response);
+    const event = new CustomEvent('locationFound', {
+      detail: {
+        coordinates: response.latt_long.split(','),
+        abbreviation: response.consolidated_weather[0].weather_state_abbr
+      }
+    });
+    window.dispatchEvent(event);
   })
-};
+}
 
 var app = function(){
   const mapWrapper = new MapWrapper("map", 51.5074, 0.1278, 10);
-
-  mapWrapper.map.on('load', function (event) {
-    const lat = event.latlng.lat;
-    const lng = event.latlng.lng;
-    console.log(lat);
-    const weatherurl = `https://www.metaweather.com/api/location/search/?lattlong=${lat},${lng}`;
-    makeRequest(weatherurl, requestComplete);
-  });
+  window.addEventListener('locationFound', function(e) {
+    mapWrapper.addMarker(e.detail.coordinates, e.detail.abbreviation);
+    mapWrapper.moveTo(e.detail.coordinates);
+  })
 };
+
+const searchOnSubmit = function(event) {
+  event.preventDefault(); // prevents page from reloading upon submit
+  const cityName = document.getElementById('location-search').value;
+  if(!cityName) throw new Error('Empty city name');
+  const weatherurl = `https://www.metaweather.com/api/location/search/?query=${cityName}`;
+  makeRequest(weatherurl, fetchWeatherData);
+}
 
 window.addEventListener('load', app);
--- public/mapWrapper.js
+++ public/mapWrapper.js
@@ -1,16 +1,18 @@
 const MapWrapper = function(element, lat, lng, zoom){
   const osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
   const osm = new L.TileLayer(osmUrl);
-
   this.map = L.map(element).addLayer(osm).setView([lat, lng], zoom);
-  this.map.on("click", this.handleMapClick.bind(this));
 };
 
-MapWrapper.prototype.handleMapClick = function (event) {
-  // debugger;
-  this.addMarker(event.latlng.lat, event.latlng.lng);
+const weatherIconProvider = function (abbreviation) {
+  return L.icon( {iconUrl:`https://www.metaweather.com/static/img/weather/${abbreviation}.svg`,  iconSize: [50, 100]});
+}
+
+MapWrapper.prototype.addMarker = function (coords, abbreviation) {
+  L.marker(coords, { icon: weatherIconProvider(abbreviation) }).addTo(this.map);
 };
 
-MapWrapper.prototype.addMarker = function(lat, lng, text){
-  L.marker([lat, lng]).bindPopup(text).addTo(this.map);
+MapWrapper.prototype.moveTo = function (coords) {
+  console.log('COORDS', coords);
+  this.map.panTo(coords, 6);
 };
